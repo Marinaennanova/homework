37778cd9f225c60b91f19e4ec88a6c7a8d9d0f7d
Ennanova Marina <clavicorda@yandex.ru>
2024-11-02 19:37:54 +0300

    02/11/2024

---
+++ Вывод и ввод.py
@@ -0,0 +1,10 @@
+name=("Университет")
+print(name)
+print(type(name)) # данная команда как раз и выводит тип переменной,тип данной переменной str
+age=(44)
+print(age)
+print(type(age)) # вывод типа переменной age,тип переменной int
+age=(44+10)
+print(age) # в данном случае произошло сложение значений одинакового типа
+is_student=True
+print(type(is_student)) # класс переменной bool
